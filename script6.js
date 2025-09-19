a7e37bc17483c307cbe5b0138931cb2a08a791d1
JOSÉ AZARÍAS PÉREZ TORRES <jose_a_torres@estudante.sesisenai.org.br>
2025-09-18 21:49:32 -0300

    Arrumando erros

--- script6.js
+++ script6.js
@@ -9,8 +9,4 @@ console.log("o lanche "+ lanche.nome + " custa " + lanche.preco);
 
 lanche.preco = 17.50;
 
-console.log(lanche)
-
-console.log ("Amo homenzinhos e o corinthians");
-console.log ("Só uso o chatGPT pra rodar");
-console.log ("Amo dar");
+console.log(lanche);
